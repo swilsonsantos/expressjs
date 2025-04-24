a9c311fdfa8f9e3514e865b1dfb4ce24b1bf81d8
Sérgio Santos <swilsonsantos@gmail.com>
2025-04-24 02:32:44 +0100

    Make changes to allow render to make it run

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (_req, res) => {
   res.json({ message: 'Hello World!' });
